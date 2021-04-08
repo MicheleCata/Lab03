64b64c907f2ec9bd9b90b7c8d0365a4c84de5c20
Michele <Michele@localhost>
2021-04-08 13:20:22 +0200

    Esercizio 1 completo

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -3,49 +3,36 @@ package it.polito.tdp.spellchecker.model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Dictionary {
 	
-	List <RichWord> listaParoleEN;
-	List <RichWord> listaParoleIN;
+	private List <String> dizionario;
+	private String lingua;
 	
-	public Dictionary() {
-		listaParoleEN = new LinkedList <>();
-		listaParoleIN = new LinkedList <>();
-	}
-	
-	public void loadDictionary (String language) {
-		if (language.equals("English")) {
+	public boolean loadDictionary (String language) {
+		if (dizionario!=null && this.lingua.equals(language)) {
+			return true;
+		}
+		dizionario= new LinkedList <>();
+		this.lingua=language;
 		try {
-			FileReader fr = new FileReader ("src/main/resources/English.txt");
+			FileReader fr = new FileReader ("src/main/resources/" + language +".txt");
 			BufferedReader br = new BufferedReader(fr);
 			String word;
 			while ((word = br.readLine())!=null) {
-				RichWord rw = new RichWord(word);
-				listaParoleEN.add(rw);
+				
+				dizionario.add(word);
 			}
+			Collections.sort(dizionario);
 			br.close();
+			return true;
 		}catch (IOException e) {
 			System.out.println("Errore nella lettura da file");
+			return false;
 		}
-	}
-		else if (language.equals("Italian")) {
-			try {
-				FileReader fr = new FileReader ("src/main/resources/Italian.txt");
-				BufferedReader br = new BufferedReader(fr);
-				String word;
-				while ((word = br.readLine())!=null) {
-					
-					RichWord rw = new RichWord(word);
-					listaParoleIN.add(rw);
-				}
-				br.close();
-			}catch (IOException e) {
-				System.out.println("Errore nella lettura da file");
-			}
-			}
 	}
 	
 	public List<String> creaListaStringhe(String s) {
@@ -63,11 +50,13 @@ public class Dictionary {
 		for (String s: inputTextList) {
 			String ss= s.replaceAll("[.,\\/#!?$%\\*;:{}=\\-_`~()\\[\\]\"]","").toLowerCase();
 			rw = new RichWord(ss);
-			listaRw.add(rw);
-			if (listaParoleEN.contains(rw) || listaParoleIN.contains(rw)) 
+			
+			if (dizionario.contains(ss)) 
 				rw.setCorretta(true);
 			else
 				rw.setCorretta(false);
+			
+			listaRw.add(rw);
 		}
 		return listaRw;
 	}
